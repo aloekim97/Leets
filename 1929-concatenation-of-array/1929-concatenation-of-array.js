d6ee771e16a09da6bd67cf877d88209bf89c6535
aloekim97 <aloekim97@gmail.com>
2023-03-23 17:27:43 -0700

    Time: 74 ms (80.24%), Space: 45.9 MB (34.67%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    let a = []
+     for (let i = 0; i < nums.length; i++) {
+         a.push(nums[i])
+     }
+    return nums.concat(a)
+};
