831ef441be52b55ece09fdea7b6146c8d2a14f46
aloekim97 <aloekim97@gmail.com>
2023-04-16 20:25:27 -0700

    Time: 77 ms (66.02%), Space: 46.9 MB (54.43%) - LeetHub

---
+++ 0929-unique-email-addresses/0929-unique-email-addresses.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} emails
+ * @return {number}
+ */
+
+var numUniqueEmails = function(emails) {
+  const valid=emails.map(email=>{
+       const [local, domain] = email.split('@');
+       return local.split('+').shift().split('.').join('')+'@'+domain;
+   });
+   const set=new Set(valid)
+    
+    return set.size
+      
+    
+};
