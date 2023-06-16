bdf7193f9ecb1cffeb7643d395f898e78c47df92
aloekim97 <aloekim97@gmail.com>
2023-06-16 14:33:46 -0700

    Time: 70 ms (23.70%), Space: 43.1 MB (35.55%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+        let [left, right] = [0, numbers.length - 1];
+
+    while (left < right) {
+        const sum = numbers[left] + numbers[right];
+
+        const isTarget = sum === target;
+        if (isTarget) return [left + 1, right + 1];
+
+        const isTargetGreater = sum < target;
+        if (isTargetGreater) left++;
+
+        const isTargetLess = target < sum;
+        if (isTargetLess) right--;
+    }
+
+    return [-1, -1];
+};
